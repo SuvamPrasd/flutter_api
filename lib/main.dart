ad7a53a3ede92860230d67661b4118ec950cac18
suvam prasad <tituprasad30@gmail.com>
2020-05-30 10:51:06 +0530

    fetch func added

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+import 'dart:convert';
 
 import './models/album.dart';
 
@@ -30,5 +32,16 @@ class _ApiFlutterState extends State<ApiFlutter> {
         child: Text('Some data'),
       ),
     );
+  }
+}
+
+Future<Album> fetchAlbum() async {
+  final response =
+      await http.get('https://jsonplaceholder.typicode.com/albums/1');
+
+  if (response.statusCode == 200) {
+    return Album.fromJson(json.decode(response.body));
+  }else{
+    throw Exception('Failed to load album')
   }
 }
