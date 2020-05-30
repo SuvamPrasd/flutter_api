da92955a6f12ef3e418484737a0d9f5ab516da64
suvam prasad <tituprasad30@gmail.com>
2020-05-30 10:24:12 +0530

    flutter files

---
+++ lib/main.dart
@@ -0,0 +1,30 @@
+import 'package:flutter/material.dart';
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter_api',
+      home: ApiFlutter(),
+    );
+  }
+}
+
+class ApiFlutter extends StatefulWidget {
+  @override
+  _ApiFlutterState createState() => _ApiFlutterState();
+}
+
+class _ApiFlutterState extends State<ApiFlutter> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Flutte Api'),
+      ),
+      body: Center(
+        child: Text('Some data'),
+      ),
+    );
+  }
+}
