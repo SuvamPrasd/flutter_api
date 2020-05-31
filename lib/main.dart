43c81df7be98d333a6f9dc440dd9e86aae7fa243
suvam prasad <tituprasad30@gmail.com>
2020-05-31 18:01:41 +0530

    animation added

--- lib/main.dart
+++ lib/main.dart
@@ -22,92 +22,180 @@ class ApiFlutter extends StatefulWidget {
   _ApiFlutterState createState() => _ApiFlutterState();
 }
 
-class _ApiFlutterState extends State<ApiFlutter> {
+class _ApiFlutterState extends State<ApiFlutter> with TickerProviderStateMixin {
   Future<Album> futureAlbum;
   Future<Nasa> futureResult;
+  AnimationController _controller;
+  Animatable<Color> background = TweenSequence<Color>(
+    [
+      TweenSequenceItem(
+        weight: 1.0,
+        tween: ColorTween(
+          begin: Colors.red,
+          end: Colors.green,
+        ),
+      ),
+      TweenSequenceItem(
+        weight: 1.0,
+        tween: ColorTween(
+          begin: Colors.green,
+          end: Colors.blue,
+        ),
+      ),
+      TweenSequenceItem(
+        weight: 1.0,
+        tween: ColorTween(
+          begin: Colors.blue,
+          end: Colors.pink,
+        ),
+      ),
+    ],
+  );
 
   @override
   void initState() {
     super.initState();
     futureAlbum = fetchAlbum();
     futureResult = fetchData();
+    _controller = AnimationController(
+      duration: const Duration(seconds: 10),
+      vsync: this,
+    )..repeat();
     print(futureResult);
   }
 
   @override
+  void dispose() {
+    // TODO: implement dispose
+    super.dispose();
+    _controller.dispose();
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return Scaffold(
-        appBar: AppBar(
-          title: Text('Flutte Api'),
-        ),
-        body: FutureBuilder<Nasa>(
-          future: futureResult,
-          builder: (ctx, snapshot) {
-            if (snapshot.hasData) {
-              return SingleChildScrollView(
-                child: Container(
-                  margin: EdgeInsets.symmetric(
-                    horizontal: 10,
-                    vertical: 10,
-                  ),
-                  child: Card(
-                    child: Padding(
-                      padding: const EdgeInsets.all(20.0),
-                      child: Column(
-                        children: <Widget>[
-                          Text(
-                            snapshot.data.title,
-                            style: TextStyle(
-                              fontWeight: FontWeight.bold,
-                              fontSize: 30,
-                            ),
-                          ),
-                          SizedBox(
-                            height: 12,
-                          ),
-                          Text(
-                            snapshot.data.content,
-                            style: TextStyle(
-                              fontWeight: FontWeight.w300,
-                              letterSpacing: 0.4,
-                              height: 1.5,
-                            ),
-                          ),
-                          SizedBox(
-                            height: 12,
-                          ),
-                          Image.network(
-                            snapshot.data.imageUrl,
-                            fit: BoxFit.cover,
-                            loadingBuilder: (ctx, child, loadingProgrss) {
-                              if (loadingProgrss == null) {
-                                return child;
-                              }
-                              return Center(
-                                child: LinearProgressIndicator(
-                                  value: loadingProgrss.expectedTotalBytes !=
-                                          null
-                                      ? loadingProgrss.cumulativeBytesLoaded /
-                                          loadingProgrss.expectedTotalBytes
-                                      : null,
-                                ),
-                              );
-                            },
-                          )
-                        ],
-                      ),
-                    ),
+    return AnimatedBuilder(
+        animation: _controller,
+        builder: (ctx, builder) {
+          return Scaffold(
+              backgroundColor: Colors.black,
+              appBar: AppBar(
+                title: Text(
+                  'Flutte API',
+                  style: TextStyle(
+                    fontWeight: FontWeight.w300,
+                    letterSpacing: 1,
                   ),
                 ),
-              );
-            } else if (snapshot.hasError) {
-              return Text("${snapshot.error}");
-            }
+                centerTitle: true,
+                backgroundColor: Colors.black,
+              ),
+              body: FutureBuilder<Nasa>(
+                future: futureResult,
+                builder: (ctx, snapshot) {
+                  if (snapshot.hasData) {
+                    return SingleChildScrollView(
+                      child: Container(
+                        padding: EdgeInsets.symmetric(
+                          horizontal: 10,
+                          vertical: 10,
+                        ),
+                        color: Colors.black,
+                        child: Card(
+                          color: background.evaluate(
+                              AlwaysStoppedAnimation(_controller.value)),
+                          child: Padding(
+                            padding: const EdgeInsets.all(20.0),
+                            child: Column(
+                              children: <Widget>[
+                                Text(
+                                  snapshot.data.title,
+                                  style: TextStyle(
+                                    fontWeight: FontWeight.bold,
+                                    fontSize: 30,
+                                  ),
+                                ),
+                                SizedBox(
+                                  height: 12,
+                                ),
+                                Image.network(
+                                  snapshot.data.imageUrl,
+                                  fit: BoxFit.cover,
+                                  loadingBuilder: (ctx, child, loadingProgrss) {
+                                    if (loadingProgrss == null) {
+                                      return child;
+                                    }
+                                    return LinearProgressIndicator(
+                                      backgroundColor: Colors.white,
+                                      valueColor:
+                                          AlwaysStoppedAnimation(Colors.black),
+                                      value: loadingProgrss
+                                                  .expectedTotalBytes !=
+                                              null
+                                          ? loadingProgrss
+                                                  .cumulativeBytesLoaded /
+                                              loadingProgrss.expectedTotalBytes
+                                          : null,
+                                    );
+                                  },
+                                ),
+                                SizedBox(
+                                  height: 12,
+                                ),
+                                Text(
+                                  snapshot.data.content,
+                                  style: TextStyle(
+                                    fontWeight: FontWeight.w300,
+                                    letterSpacing: 0.4,
+                                    height: 1.5,
+                                    color: Colors.white,
+                                  ),
+                                ),
+                                SizedBox(
+                                  height: 12,
+                                ),
+                                OutlineButton.icon(
+                                    onPressed: () {
+                                      if (_controller.isAnimating) {
+                                        _controller.stop();
+                                      } else {
+                                        _controller.repeat();
+                                      }
+                                    },
+                                    icon: Icon(Icons.stop),
+                                    label: Text('Stop Animation')),
+                              ],
+                            ),
+                          ),
+                        ),
+                      ),
+                    );
+                  } else if (snapshot.hasError) {
+                    return Text("${snapshot.error}");
+                  }
 
-            // By default, show a loading spinner.
-            return CircularProgressIndicator();
-          },
-        ));
+                  // By default, show a loading spinner.
+                  return Center(
+                      child: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: <Widget>[
+                      CircularProgressIndicator(
+                        backgroundColor: Colors.white,
+                        valueColor: AlwaysStoppedAnimation(Colors.black),
+                      ),
+                      SizedBox(
+                        height: 10,
+                      ),
+                      Text(
+                        'Loading...',
+                        style: TextStyle(
+                          color: Colors.white,
+                        ),
+                      ),
+                    ],
+                  ));
+                },
+              ));
+        });
   }
 }
 
