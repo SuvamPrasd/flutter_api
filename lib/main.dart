46b53f2c4a428b1b5592a897030016714dea5242
suvam prasad <tituprasad30@gmail.com>
2020-05-31 16:07:24 +0530

    fetchData func added

--- lib/main.dart
+++ lib/main.dart
@@ -3,6 +3,7 @@ import 'package:http/http.dart' as http;
 import 'dart:convert';
 
 import './models/album.dart';
+import './models/nasa.dart';
 
 void main() => runApp(MyApp());
 
@@ -23,11 +24,14 @@ class ApiFlutter extends StatefulWidget {
 
 class _ApiFlutterState extends State<ApiFlutter> {
   Future<Album> futureAlbum;
+  Future<Nasa> futureResult;
 
   @override
   void initState() {
-    futureAlbum = fetchAlbum();
     super.initState();
+    futureAlbum = fetchAlbum();
+    futureResult = fetchData();
+    print(futureResult);
   }
 
   @override
@@ -36,8 +40,8 @@ class _ApiFlutterState extends State<ApiFlutter> {
         appBar: AppBar(
           title: Text('Flutte Api'),
         ),
-        body: FutureBuilder(
-          future: futureAlbum,
+        body: FutureBuilder<Nasa>(
+          future: futureResult,
           builder: (ctx, snapshot) {
             if (snapshot.hasData) {
               return Text(snapshot.data.title);
@@ -53,6 +57,8 @@ class _ApiFlutterState extends State<ApiFlutter> {
 }
 
 Future<Album> fetchAlbum() async {
+  const BASE_URL =
+      "https://api.nasa.gov/planetary/apod?api_key=gKLuukaxhBSWbNFPeeR7UcLC84ptiIPxWvScNJ2S";
   final response =
       await http.get('https://jsonplaceholder.typicode.com/albums/1');
 
@@ -60,5 +66,17 @@ Future<Album> fetchAlbum() async {
     return Album.fromJson(json.decode(response.body));
   } else {
     throw Exception('Failed to load album');
+  }
+}
+
+Future<Nasa> fetchData() async {
+  final nasa = Nasa();
+  const BASE_URL =
+      "https://api.nasa.gov/planetary/apod?api_key=gKLuukaxhBSWbNFPeeR7UcLC84ptiIPxWvScNJ2S";
+  final response = await http.get(BASE_URL);
+  if (response.statusCode == 200) {
+    return nasa.fetchNasa(json.decode(response.body));
+  } else {
+    throw Exception('Failed to load');
   }
 }
